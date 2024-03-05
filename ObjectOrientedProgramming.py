b92d89a134a39f14ce8a8b895e974f1b1d8c20fb
Parth Zala <parthzala303@gmail.com>
2024-03-05 10:21:48 +0530

    Pushing updated inheritance code

--- ObjectOrientedProgramming.py
+++ ObjectOrientedProgramming.py
@@ -1,5 +1,5 @@
 class test:
-    def __init__(self,name,age,hobby): #The __init__() function is called automatically every time the class is being used to create a new object.
+    def __init__(self,name,age,hobby): #The __init__()called (as constructor) is called automatically every time the class is being used to create a new object.
         self.name=name
         self.age=age
         self.hobby = hobby
@@ -7,16 +7,24 @@ class test:
         print("My name is ",self.name)
         print("My hobby is ",self.hobby)
         print("My age is ",self.age)
-obj1= test("Parth",23,"Football")
-# obj1.display()
-#Bank account project
 
-class account:
-    account_number=12345
-    balance=0
-    def __init__(self,balance,deposite):
-        self.balance=balance
-        self.deposite=deposite
-    def current_balance(self,balance,deposite):
-        balance=balance+deposite
-        
+
+#This is a one simple example of how to create a class and create object and call the objects of the class
+
+#Inheritance : When object of the parent class is called by child class
+#https://www.geeksforgeeks.org/inheritance-in-python/
+
+class child(test):
+    # def __init__(self,name,age,hobby):
+    #     # super().__init__(name,age,hobby)
+    #     pass
+    def child_function(self,name,age,hobby,new_property):
+        print('My name is :', name)
+        print("My hobby is : ",hobby)
+        print("My age is : ",age)
+        print("New",new_property)
+# obj1= test("Parth",23,"Football")
+# obj1.display()    
+obj2=child("Zala",69,"Movies")  
+obj2.display() 
+obj2.child_function("Zala",69,"Movies",'j')    
